ccc6e8b91cdb9d0c1f163025bddb0071d8806dca
tsirkulok <tsirkul@DESKTOP-D5P8PFA.localdomain>
2026-03-29 07:41:56 +0000

    Final version: added exit logic, fixed switch cases and cleaned up code

--- logic.js
+++ logic.js
@@ -21,34 +21,44 @@ function div(num1,num2) {
     return num1 / num2;
 }
 async function calc() {
-    const number1 = await rl.question("write the first number: ");
+    while(true){
+        const number1 = await rl.question("\u001b[34mwrite the first number: \u001b[0m");
     
-    const number2 = await rl.question("write the second number: ");
+        const number2 = await rl.question("\u001b[34mwrite the second number: \u001b[0m");
     
-    const oper = await rl.question("write the operator: ");
-    const n1 = parseFloat(number1);
-    const n2 = parseFloat(number2);
-    if (isNaN(n1) || isNaN(n2)) {
-        console.log("you must write the number!")
-        rl.close()
-        return
-    } 
+        const oper = await rl.question("\u001b[34mwrite the operator: \u001b[0m");
+        const n1 = parseFloat(number1);
+        const n2 = parseFloat(number2);
+        if (isNaN(n1) || isNaN(n2)) {
+            console.log("\u001b[31myou must write the number!\u001b[0m")
+            continue
+            ''
+        } 
     
     
-    switch (oper) {
+        switch (oper) {
         
-        case "+":
-            console.log(`result is:${sum(n1,n2)}`);
-            break;
-        case "-":
-            console.log(`result is:${sub(n1,n2)}`);
-            break;
-        case "*":
-            console.log(`result is:${mul(n1,n2)}`);
-            break;       
-        case "/":
-            console.log(`result is:${div(n1,n2)}`)     
-    
+            case "+":
+                console.log(`\u001b[32mresult is:${sum(n1,n2)}\u001b[0m`);
+                break;
+            case "-":
+                console.log(`\u001b[32mresult is:${sub(n1,n2)}\u001b[0m`);
+                break;
+            case "*":
+                console.log(`\u001b[32mresult is:${mul(n1,n2)}\u001b[0m`);
+                break;           
+            case "/":
+                console.log(`\u001b[32mresult is:${div(n1,n2)}\u001b[0m`)     
+                break;
+            default:
+                console.log("\u001b[31mще не додано :)\u001b[0m")
+                break;    
         }
-rl.close()}
+        let dream = await rl.question("\u001b[33mбажаєте вийти(y or n)?\u001b[0m\n")   
+        if(dream === "y") {
+            rl.close()
+            break;
+    }
+        }
+    }
 calc();
